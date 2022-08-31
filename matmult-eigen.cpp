b40890a4e3515a6c2f31826a896ea887a14ef7b6
Heechul Yun <heechul.yun@ku.edu>
2022-08-31 16:58:42 -0500

    bug fixes.

--- matmult-eigen.cpp
+++ matmult-eigen.cpp
@@ -60,36 +60,41 @@ int main(int argc, char *argv[])
 
 }
 
+int get_random_int(int min, int max)
+{
+  // std::uniform_real_distribution<int> dis(min, max);
+  // std::default_random_engine re;
+  // return dis(re);
+  return (rand() % (max - min)) + min; 
+}
+
+
 std::vector<long> gen_random_sample(long min, long max, long sample_size)
 {
   std::vector<long> my_vector(sample_size); // THE BUG, is right std::vector<long> my_vector
 
-  for (long i = min; i != max; i++)
-    {
-      my_vector.push_back(i);
-    }
-
-  std::random_shuffle(my_vector.begin(), my_vector.end());
-
-  std::vector<long> new_vec = std::vector<long>(my_vector.begin(), my_vector.begin() + sample_size);
-
-    return new_vec;
+  for (long i = 0; i < sample_size; i++) {
+    my_vector[i] = get_random_int((int)min, (int)max);
+    // std::cout << my_vector[i] << std::endl;
+  }
+  return my_vector;
 }
 
 double get_random_double(double min, double max)
 {
-   std::uniform_real_distribution<double> unif(min, max);
-   std::default_random_engine re;
-   double a_random_double = unif(re);
-   return a_random_double;
+  std::random_device rd;  // Will be used to obtain a seed for the random number engine
+  std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
+  std::uniform_real_distribution<> dis(min, max);
+  double a_random_double = dis(gen);
+  return a_random_double;
 }
 
 std::vector<double> get_vector_of_rn_doubles(int length, double min, double max)
 {
   std::vector<double> my_vector(length);
-  for (int i=0; i < length; i++)
-    {
-      my_vector[i] = get_random_double(min, max);
-    }
+  for (int i=0; i < length; i++) {
+    my_vector[i] = get_random_double(min, max);
+    // std::cout << my_vector[i] << std::endl;
+  }
   return my_vector;
 }
