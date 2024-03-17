5eb7cb67850ff7bc32f0456739c41e6a885cef50
Heechul Yun <heechul.yun@ku.edu>
2024-03-17 17:55:11 -0500

    cleanup

--- test-transpose.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-algo=0
-for n in 16 32 64 128 256 512 1024 2048 4096; do 
-    ./matrix -n $n -a $algo > tmp.txt
-    dur=`grep secs tmp.txt | awk '{ print $3 }'`
-    ws=`expr $n \* $n \* 4 \* 3 / 1024`
-    echo $n $ws $dur
-done
